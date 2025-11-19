a18ee2f6b142ed20dae31ffbc4d384e543544d2c
nehah03 <nehah888999@gmail.com>
2025-11-19 19:32:00 +0530

    fix code

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,12 @@
-import React from 'react-dom';
+import React from 'react';
+import { createRoot } from 'react-dom/client';
 import App from './App';
 import './index.css';
 
-const root = document.getElementById('root');
-if (root) {
-  React.createRoot(root).render(
+const container = document.getElementById('root');
+if (container) {
+  const root = createRoot(container);
+  root.render(
     <React.StrictMode>
       <App />
     </React.StrictMode>
